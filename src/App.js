8bf0176a2a97c8b36465331441806ddf34d7cb05
znewton <znewton@iastate.edu>
2017-06-12 17:21:22 -0500

    Initial create-react-app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './sass/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
